79b15b3db277a884175b44193a61f95b6d581210
mechauk418 <108647501+mechauk418@users.noreply.github.com>
2022-09-04 14:43:13 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 227 ms, Memory: 62,732 KB -BaekjoonHub

---
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -0,0 +1,29 @@
+
+T = int(input())
+
+for t in range(1,T+1):
+
+
+    N = int(input())
+
+    N_list=[0] * 5000
+    C_list = []
+
+    for i in range(N):
+        A,B = map(int,input().split())
+        for j in range(A-1,B):
+            N_list[j]+=1
+
+
+    P = int(input())
+
+    for i in range(P):
+        C = int(input())
+        C_list.append(C)
+
+    print(f'#{t}',end=' ')
+    for c in C_list:
+        print(f'{ N_list[c-1]}',end=' ')
+        
+    print()
+
