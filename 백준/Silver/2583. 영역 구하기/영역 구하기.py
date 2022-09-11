5e5b0dc353a28a142addab9b57ab56c290ac4e87
mechauk418 <108647501+mechauk418@users.noreply.github.com>
2022-09-11 23:18:35 +0900

    [Silver I] Title: 영역 구하기, Time: 100 ms, Memory: 40688 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,54 @@
+
+import copy,sys
+sys.setrecursionlimit(10**6)
+
+def dfs(x,y):
+
+    if x<0 or x>=M or y<0 or y>=N:
+        return False
+
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        global cnt
+        cnt+=1
+        dfs(x+1, y)
+        dfs(x-1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+
+        return True
+
+    return False
+
+
+M,N,K = map(int,input().split())
+
+graph = [  [0 for _ in range(N)] for _ in range(M)    ]
+
+
+for _ in range(K):
+    a,b,c,d = map(int,input().split())
+
+    for i in range(b,d):
+        for j in range(a,c):
+            graph[i][j] = 1
+
+
+count_list = []
+result = 0
+
+for i in range(M):
+    for j in range(N):
+        cnt = 0
+
+        if dfs(i,j) == True:
+            result +=1
+            count_list.append(cnt)
+            cnt=0
+
+print(result)
+count_list.sort()
+for i in count_list:
+    print(i,end=' ')
+
