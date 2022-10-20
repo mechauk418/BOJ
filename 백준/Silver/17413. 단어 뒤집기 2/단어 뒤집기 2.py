42c38e2c3130e3defe7746807a5e89ea3e10b047
mechauk418 <108647501+mechauk418@users.noreply.github.com>
2022-10-20 23:51:33 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 104 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,23 @@
+import sys
+word = list(sys.stdin.readline().rstrip())
+
+i = 0
+start = 0
+
+while i < len(word):
+    if word[i] == "<":
+        i += 1
+        while word[i] != ">":
+            i += 1
+        i += 1
+    elif word[i].isalnum():
+        start = i
+        while i < len(word) and word[i].isalnum():
+            i+=1
+        tmp = word[start:i]
+        tmp.reverse()
+        word[start:i] = tmp
+    else:
+        i+=1                
+
+print("".join(word))
