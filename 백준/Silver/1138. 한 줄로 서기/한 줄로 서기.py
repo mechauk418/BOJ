8b18313b76ed930b345ee73b552fb767ce64f760
mechauk418 <mechauk418@gmail.com>
2022-11-21 22:16:06 +0900

    [Silver II] Title: 한 줄로 서기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,17 @@
+n = int(input())
+h = list(map(int, input().split()))
+ans = [0] * n
+for p in range(1, n+1):
+    t = h[p-1]
+    cnt = 0
+    for i in range(n):
+        if cnt == t and ans[i] == 0:
+            ans[i] = p
+            break
+        elif ans[i] == 0:
+            cnt += 1
+print(*ans)
+
+
+
+
