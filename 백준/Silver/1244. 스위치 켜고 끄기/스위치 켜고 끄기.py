5750349162367cbd3648d81277527b0c604d0074
mechauk418 <mechauk418@gmail.com>
2022-12-29 10:12:10 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -0,0 +1,47 @@
+
+N = int(input())
+S_list = list(map(int,input().split()))
+
+P = int(input())
+
+P_list = []
+
+for i in range(P):
+
+    G,S = map(int,input().split())
+    P_list.append((G,S))
+
+
+for i in P_list:
+
+    if i[0] == 1:
+        for j in range(1,(N//i[1])+1):
+            idx = i[1]*j # 3 6
+            S_list[idx-1]=1-S_list[idx-1]
+
+    else:
+        rng = min(i[1]-1,N-i[1]) # 3
+        S_list[i[1] - 1] = 1 - S_list[i[1] - 1]
+        for k in range(1,rng+1):
+            if S_list[i[1]-1-k]==S_list[i[1]-1+k]:
+                S_list[i[1] - 1 - k] = 1-S_list[i[1]-1-k]
+                S_list[i[1] - 1 + k] = 1-S_list[i[1]-1+k]
+            else:
+                break
+
+
+ans_list = []
+ans = ''
+for i in S_list:
+    ans += str(i)
+
+    if len(ans)==20:
+        ans_list.append(ans)
+        ans=''
+
+ans_list.append(ans)
+
+for i in ans_list:
+    for j in i:
+        print(j,end=' ')
+    print()
