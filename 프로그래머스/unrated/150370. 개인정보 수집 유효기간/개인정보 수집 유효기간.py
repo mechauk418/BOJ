0ae4e932cd435afbdcbf6de272ed8b1b52602285
mechauk418 <mechauk418@gmail.com>
2023-02-16 10:32:17 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,39 +1,47 @@
 def solution(today, terms, privacies):
-    answer = []
-    term_dict = dict()
-    today_year,today_month,today_day = map(int,today.split('.'))
-    idx = 1
-    
+    # 약관 종류: terms[0][0], terms[1][0], ...
+    # 약관 유효기간: terms[0][2], terms[1][2], ...
 
-    for t in terms:
-        alp,num = t.split()
-        term_dict[alp] = int(num)
-        
-    term_alp = list(term_dict.keys())
-    
-    for pri in privacies:
-        priday,term = pri.split()
-        year,month,day = map(int,priday.split('.'))
-        for alp in term_alp:
-            if term ==alp:
-                month+=term_dict[alp]
-                while True:
-                    
-                    if month >12:
-                        year+=1
-                        month = month-12
-                    else:
-                        break
-        
-        if today_year > year:
-            answer.append(idx)
+    # 개인정보 약관 종류: privacies[i][-1]
+    # ymd = (privacies[i].split()[0].split('.'))        개인정보 년, 달, 일
+    result = []
 
-        elif (year==today_year) and (today_month>month):
-            answer.append(idx)
-        elif (year==today_year) and (today_month==month) and (today_day>=day):
-            answer.append(idx)            
+    for i in range(len(privacies)):
+        today_y = int(today[0:4])                       # 오늘
+        today_m = int(today[5:7])
+        today_d = int(today[8:])
 
-        idx+=1
-    
-        
-    return answer
+        y, m, d = (privacies[i].split()[0]).split('.')  # 개인정보
+        y = int(y)
+        m = int(m)
+        d = int(d)
+
+        for j in range(len(terms)):
+            if terms[j][0] == privacies[i][-1]:
+                d += int(terms[j][2:]) * 28 - 1
+
+        if d > 28:
+            m += d//28
+            d = d % 28
+            if d == 0:
+                m -= 1
+                d = 28
+                if m == 0:
+                    y -= 1
+                    m = 12
+            if m > 12:
+                y += m // 12
+                m = m % 12
+                if m==0:
+                    m=12
+                    y -= 1
+
+        if today_y > y:
+            result.append(i+1)
+            continue
+        elif today_y == y and today_m > m:
+            result.append(i+1)
+            continue
+        elif today_y == y and today_m == m and today_d > d:
+            result.append(i+1)
+    return result
