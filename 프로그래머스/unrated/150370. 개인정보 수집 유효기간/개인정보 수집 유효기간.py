3337dc29057f577e16ad74a381a31b24c4a1375d
mechauk418 <mechauk418@gmail.com>
2023-02-16 10:03:47 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,38 @@
+def solution(today, terms, privacies):
+    answer = []
+    term_dict = dict()
+    today_year,today_month,today_day = map(int,today.split('.'))
+    idx = 1
+    
+
+    for t in terms:
+        alp,num = t.split()
+        term_dict[alp] = int(num)
+        
+    term_alp = list(term_dict.keys())
+    for pri in privacies:
+        priday,term = pri.split()
+        year,month,day = map(int,priday.split('.'))
+        for alp in term_alp:
+            if term ==alp:
+                month+=term_dict[alp]
+                while True:
+                    
+                    if month >12:
+                        year+=1
+                        month = month-12
+                    else:
+                        break
+        
+        if today_year > year:
+            answer.append(idx)
+
+        elif (year==today_year) and today_month>month:
+            answer.append(idx)
+        elif (year==today_year) and (today_month==month) and (today_day>=day):
+            answer.append(idx)            
+
+        idx+=1
+    
+        
+    return answer
