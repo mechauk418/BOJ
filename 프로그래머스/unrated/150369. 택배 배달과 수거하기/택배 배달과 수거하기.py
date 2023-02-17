cdad9a7d7ab64ff0c062a2763f903daf65dcfc91
mechauk418 <mechauk418@gmail.com>
2023-02-17 10:26:16 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 48.46 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,21 +1,12 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-
-    d = 0
-    p = 0
-
+    # 역순으로 누적합
+    for i in range(n-2, -1, -1):
+        deliveries[i] += deliveries[i+1]
+        pickups[i] += pickups[i+1]
+    k = 0
     for i in range(n-1, -1, -1):
-
-        cnt = 0
-
-        d -= deliveries[i]
-        p -= pickups[i]
-
-        while d < 0 or p < 0:
-            d += cap
-            p += cap
-            cnt += 1
-
-        answer += (i + 1) * 2 * cnt
-
+        while deliveries[i] > cap*k or pickups[i] > cap*k:
+            answer += (i+1)*2
+            k += 1
     return answer
