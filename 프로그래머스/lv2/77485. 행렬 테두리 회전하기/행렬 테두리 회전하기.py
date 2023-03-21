590dd4054e42aec5720aee927af89445670dfb9d
mechauk418 <mechauk418@gmail.com>
2023-03-21 10:31:26 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 207.10 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,6 +1,7 @@
 def solution(rows, columns, queries):
     answer = []
 
+    # 숫자 리스트를 잘라서 이차원 그래프로 만듬
     num_list = [ i for i in range(1,rows*columns+1)]
     graph = []
     for i in range(rows):
@@ -11,7 +12,7 @@ def solution(rows, columns, queries):
 
         x1,y1,x2,y2 = q
 
-        rev_list = []
+        rev_list = [] # 테두리의 좌표를 담을 리스트
 
         for i in range(y1,y2):
 
@@ -29,17 +30,21 @@ def solution(rows, columns, queries):
 
             rev_list.append((i,y1))
 
-        value_list = []
+        value_list = [] # 테두리의 값을 담을 리스트
+        
+        # 테두리 리스트를 완성해줌
         for i in rev_list:
             value = graph[i[0]-1][i[1]-1]
             value_list.append(value)
 
 
-        answer.append(min(value_list))
+        answer.append(min(value_list)) # 출력해야하는 최솟값을 answer에 넣어줌
 
+        # 테두리를 한칸 회전시켜줌
         elem = rev_list.pop(0)
         rev_list.append(elem)
 
+        # 회전된 좌표를 값과 매칭해줌
         for i in range(len(rev_list)):
             graph[rev_list[i][0]-1][rev_list[i][1]-1] = value_list[i]
 
