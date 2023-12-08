89c2024b93e378c3817179cc43466de098f898e0
mechauk418 <mechauk418@gmail.com>
2023-12-08 09:47:30 +0900

    [Silver II] Title: 이항 계수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+result = 1
+for i in range(K):
+    result *= N
+    N -= 1
+
+divisor = 1
+for i in range(2, K+1):
+    divisor *= i
+
+print((result // divisor) % 10007)
