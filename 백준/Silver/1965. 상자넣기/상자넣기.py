36316d360310e86a83c30a4a0eb9ed46c75aa826
mechauk418 <mechauk418@gmail.com>
2023-12-08 10:35:51 +0900

    [Silver II] Title: 상자넣기, Time: 216 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,15 @@
+
+n = int(input())
+
+box = list(map(int,input().split()))
+
+dp = [1]*n
+
+
+for i in range(1,n):
+    for j in range(i):
+        if box[i]>box[j]:
+            dp[i]=max(dp[j]+1,dp[i])
+
+
+print(max(dp))
