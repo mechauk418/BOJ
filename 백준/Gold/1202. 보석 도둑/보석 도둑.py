e326bc62fb6ed20ee4a349e1cf90bd5be0ae0fd0
mechauk418 <mechauk418@gmail.com>
2024-04-30 11:17:19 +0900

    [Gold II] Title: 보석 도둑, Time: 2152 ms, Memory: 106592 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -0,0 +1,22 @@
+import sys
+import heapq
+
+N, K = map(int, sys.stdin.readline().split())
+jew = []
+for _ in range(N):
+    heapq.heappush(jew, list(map(int, sys.stdin.readline().split())))
+bags = []
+for _ in range(K):
+    bags.append(int(sys.stdin.readline()))
+bags.sort()
+
+answer = 0
+tmp_jew = []
+for bag in bags:
+    while jew and bag >= jew[0][0]:
+        heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
+    if tmp_jew:
+        answer -= heapq.heappop(tmp_jew)
+    elif not jew:
+        break
+print(answer)
