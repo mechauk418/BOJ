2b2dece02dae07a445d4483ff4823a25de36a766
mechauk418 <mechauk418@gmail.com>
2024-06-07 17:10:27 +0900

    [Silver II] Title: 이동하기, Time: 836 ms, Memory: 78604 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,16 @@
+import sys
+
+n,m = map(int,sys.stdin.readline().split())
+
+graph = []
+for _ in range(n):
+  graph.append(list(map(int,sys.stdin.readline().split())))
+
+dp = [[0] * (m + 1) for i in range(n + 1)]
+
+for i in range(1,n+1):
+  for j in range(1,m+1):
+    dp[i][j] = graph[i-1][j-1] + max(dp[i-1][j], dp[i][j-1],dp[i-1][j-1])
+
+
+print(dp[n][m])
